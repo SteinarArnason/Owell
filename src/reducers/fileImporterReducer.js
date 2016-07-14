096dcd71b7191d3df390cf463f3f111346ab81fe
IamStony <steinara13@ru.is>
2016-07-14 10:23:40 +0000

    changing name of state variable

--- src/reducers/fileImporterReducer.js
+++ src/reducers/fileImporterReducer.js
@@ -1,7 +1,7 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function fileImporterReducer(state = initialState.parsedFile, action) {
+export default function fileImporterReducer(state = initialState.file, action) {
     switch(action.type) {
         case types.PARSE_FILE_SUCCESS:
             return action.file;
