3c9aa3a17655c83f04bdef854a895743aefdbe1e
IamStony <steinara13@ru.is>
2016-07-18 10:34:52 +0000

    using a modified version of webpack config

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import webpack from 'webpack';
 import path from 'path';
-import config from '../webpack.config.dev';
+// import config from '../webpack.config.dev';
+import config from '../webpack.dev.config';
 import open from 'open';
 
 /* eslint-disable no-console */
