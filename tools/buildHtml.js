6b710b4b6a64e9853b441d20a2a458e20c4e54f7
IamStony <steinara13@ru.is>
2016-07-19 18:39:16 +0000

    lint fixes

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,21 +1,21 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, prefer-arrow-callback */
 import fs from 'fs';
 import cheerio from 'cheerio';
 import colors from 'colors';
 
-fs.readFile('src/index.html', 'utf8', (err, markup) => {
-  if (err) {
-    return console.log(err);
+fs.readFile('src/index.html', 'utf8', (readError, markup) => {
+  if (readError) {
+    return console.log(readError);
   }
 
   const $ = cheerio.load(markup);
 
   $('head').prepend('<link rel="stylesheet" href="styles.css" />');
 
-  fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
-    if (err) {
-      return console.log(err);
+  fs.writeFile('dist/index.html', $.html(), 'utf8', function(writeError) {
+    if (writeError) {
+      return console.log(writeError);
     }
-    console.log('index.html written to /dist'.green);
+    console.log(colors.green('index.html written to /dist'));
   });
 });
