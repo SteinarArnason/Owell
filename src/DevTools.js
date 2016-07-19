57c2376dd9984db71340df47bd0d7c7ec9fe8cb0
IamStony <steinara13@ru.is>
2016-07-19 18:18:00 +0000

    removed command that wasnt working, indent

--- src/DevTools.js
+++ src/DevTools.js
@@ -5,9 +5,9 @@ import DockMonitor from 'redux-devtools-dock-monitor';
 // import SliderMonitor from 'redux-slider-monitor'
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-h"
-    changePositionKey="ctrl-q"
-    changeMonitorKey="ctrl-m">
+  <DockMonitor
+      changePositionKey="ctrl-q"
+      toggleVisibilityKey="ctrl-h" >
     <LogMonitor />
 
   </DockMonitor>
