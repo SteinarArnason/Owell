aa564a976a7b78f344504f062f7cf2c5c4b8226d
IamStony <steinara13@ru.is>
2016-07-19 18:34:09 +0000

    lint fixes

--- src/components/fileImporter/FileImporterPage.jsx
+++ src/components/fileImporter/FileImporterPage.jsx
@@ -8,90 +8,88 @@ import styles from './FileImporterPage.css';
 
 class FileImporterPage extends Component {
 
-    // Það sem er ætlast til þess að þessi component hafi aðgang að.
-    // Þetta er síðan bundið saman í mapStateToProps() fallinu.
-    // actions þarf ekki að vera skilgreint hér því það er bundið
-    // sérstaklega saman við componentið í mapDispatchToProps() fallinu.
-    static propTypes = {
-        file: PropTypes.object.isRequired
-    }
+  // Það sem er ætlast til þess að þessi component hafi aðgang að.
+  // Þetta er síðan bundið saman í mapStateToProps() fallinu.
+  // actions þarf ekki að vera skilgreint hér því það er bundið
+  // sérstaklega saman við componentið í mapDispatchToProps() fallinu.
+  static propTypes = {
+    actions: PropTypes.object.isRequired,
+    file: PropTypes.object.isRequired
+  }
 
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        this.state = {
-            filename: 'Choose File',
-            file: {}
-        };
-
-        // Doing binds is reccomended here to improve performance on renders.
-        this.onFileSelected = this.onFileSelected.bind(this);
-        this.parseFile = this.parseFile.bind(this);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (this.props.file != nextProps.file) {
-            this.setState({file: Object.assign({}, nextProps.file)});
-        }
-    }
-
-    onFileSelected(event) {
-        const inputFile = event.target.files[0];
-
-        // Documentation for FileReader: https://developer.mozilla.org/en/docs/Web/API/FileReader
-        let reader = new FileReader();
-        reader.readAsText(inputFile);
-
-        // Fires when the reader has finished
-        reader.onload = () => {
-            const filename = inputFile.name;
-            const fileContent = JSON.stringify(reader.result);
-            this.setState({filename: filename});
-            //this.setState({file: file});
-            this.parseFile(fileContent);
-        };
-    }
-
-    parseFile(fileContent) {
-        this.props.actions.parseFile(fileContent)
-            .then((parsedFile) => {
-                // console.log(this.state);
-            })
-            .catch((error) => {
-                // console.log(error);
-            });
-    }
-
-    render() {
-        let fileSelected = this.state.filename !== 'Choose File';
-
-        return (
-            <div className="contentContainer">
-                <h1 className="blueHeader">File Importer</h1>
-                <label
-                    htmlFor="fileInput"
-                    className="labelButton" >{this.state.filename}</label>
-                <input
-                    type="file"
-                    id="fileInput"
-                    accept=".txt" // TODO Might need to be validated depending on how onFileSelected is implemented
-                    className="hideInput"
-                    onChange={this.onFileSelected} />
-
-                <br />
-            </div>
-        );
-    }
-}
-
-function mapStateToProps(state, ownProps) {
-    // console.log(state);
-    // console.log(ownProps);
-    return {
-        file: state.file || {}
+    this.state = {
+      filename: 'Choose File',
+      file: {}
     };
+
+    // Doing binds is reccomended here to improve performance on renders.
+    this.onFileSelected = this.onFileSelected.bind(this);
+    this.parseFile = this.parseFile.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.file != nextProps.file) {
+      this.setState({ file: Object.assign({}, nextProps.file) });
+    }
+  }
+
+  onFileSelected(event) {
+    const inputFile = event.target.files[0];
+
+    // Documentation for FileReader: https://developer.mozilla.org/en/docs/Web/API/FileReader
+    let reader = new FileReader();
+    reader.readAsText(inputFile);
+
+    // Fires when the reader has finished
+    reader.onload = () => {
+      const filename = inputFile.name;
+      const fileContent = JSON.stringify(reader.result);
+      this.setState({ filename }); // sama og: { filename: filename }
+      this.parseFile(fileContent);
+    };
+  }
+
+  parseFile(fileContent) {
+    this.props.actions.parseFile(fileContent)
+      .then((parsedFile) => {
+          // console.log(this.state);
+      })
+      .catch((error) => {
+          // console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="contentContainer">
+        <h1 className="blueHeader">File Importer</h1>
+        <label
+            className="labelButton"
+            htmlFor="fileInput">{this.state.filename}</label>
+        <input
+            accept=".txt" // XXX Might need to be validated depending on how onFileSelected is implemented
+            className="hideInput"
+            id="fileInput"
+            onChange={this.onFileSelected}
+            type="file" />
+
+        <br />
+      </div>
+    );
+  }
+
 }
 
+function mapStateToProps(state) {
+  // console.log(state);
+  // console.log(ownProps);
+  return {
+    file: state.file || {}
+  };
+}
 
 function mapDispatchToProps(dispatch) {
   return {
