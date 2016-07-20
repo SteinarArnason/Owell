359c0d1d069949eabe5b50d985e8a31c999b231a
IamStony <steinara13@ru.is>
2016-07-20 15:48:02 +0000

    lint fixes

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -1,14 +1,14 @@
-import * as types from '../actions/actionTypes';
+import * as constants from 'constants';
 import initialState from './initialState';
 
-function actionTypeEndsInSuccess(type) {
-  return type.substring(type.length - 8) == '_SUCCESS';
+function actionTypeEndsInSuccess(constant) {
+  return constant.substring(constant.length - 8) == '_SUCCESS';
 }
 
 export default function(state = initialState.ajaxCallsInProgress, action) {
-  if (action.type == types.BEGIN_AJAX_CALL) {
+  if (action.type == constants.BEGIN_AJAX_CALL) {
     return state + 1;
-  } else if (action.type == types.AJAX_CALL_ERROR ||
+  } else if (action.type == constants.AJAX_CALL_ERROR ||
     actionTypeEndsInSuccess(action.type)) {
     return state - 1;
   }
--- src/reducers/fileImporterReducer.js
+++ src/reducers/fileImporterReducer.js
@@ -1,9 +1,9 @@
-import * as types from '../actions/actionTypes';
+import * as constants from 'constants';
 import initialState from './initialState';
 
 export default function fileImporterReducer(state = initialState.file, action) {
   switch (action.type) {
-    case types.PARSE_FILE_SUCCESS:
+    case constants.PARSE_FILE_SUCCESS:
       return action.file;
     default:
       return state;
