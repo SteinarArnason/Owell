2a3f7a04699202942846fe73c949de46c30f0257
IamStony <steinara13@ru.is>
2016-07-20 15:37:02 +0000

    lint fixes

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,9 +4,17 @@ import { Link, IndexLink } from 'react-router';
 const Header = () => {
   return (
     <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
+      <IndexLink
+        to="/"
+        activeClassName="active">
+        { 'Home' }
+      </IndexLink>
+      { " | " }
+      <Link
+        to="/about"
+        activeClassName="active">
+        { 'About' }
+      </Link>
     </nav>
   );
 };
