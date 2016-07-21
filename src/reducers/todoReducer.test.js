30722372038ef0cd7101eaab907f0d57a5e7ed2c
IamStony <steinara13@ru.is>
2016-07-21 16:49:22 +0000

    adding reducer for TDD

---
+++ src/reducers/todoReducer.test.js
@@ -0,0 +1,29 @@
+import expect from 'expect';
+
+const todos = [
+  { id: 0, isDone: true, text: 'make components' },
+  { id: 1, isDone: false, text: 'design actions' },
+  { id: 2, isDone: false, text: 'TDD implement reducers' },
+  { id: 3, isDone: false, text: 'connect components' },
+];
+
+describe('todoReducer', () => {
+
+  it('should be the first passing test in the todoReducer', () => {
+    // arrange
+
+    // act
+
+    // assert
+    expect(true).toEqual(true);
+  });
+
+  it('should add a todo to our list of todos', () => {
+    // arrange
+
+    // act
+
+    // assert
+    expect(true).toEqual(false);
+  });
+});
