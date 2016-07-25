027a95d868776d423004460df60f7c4e5e4beb2f
IamStony <steinara13@ru.is>
2016-07-25 11:09:39 +0000

    added a toggleTodo action

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -3,3 +3,7 @@ import constants from '../constants';
 export function addTodo(todo) {
   return { type: constants.TODO.ADD, todo };
 }
+
+export function toggleTodo(id) {
+  return { type: constants.TODO.TOGGLE, id };
+}
