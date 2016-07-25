2f22bf6913b6f234868db742181cd0d077b4e467
IamStony <steinara13@ru.is>
2016-07-25 12:19:35 +0000

    removing test data

--- src/components/todo/TodoPage.jsx
+++ src/components/todo/TodoPage.jsx
@@ -62,15 +62,8 @@ class TodoPage extends Component {
 const mapStateToProps = (state, ownProps) => { //eslint-disable-line
   const todoState = state.get('todo');
 
-  const dummyTodos = [
-    { id: 0, isDone: true, text: 'make components' },
-    { id: 1, isDone: false, text: 'design actions' },
-    { id: 2, isDone: false, text: 'TDD implement reducers' },
-    { id: 3, isDone: false, text: 'connect components' },
-  ];
-
   return {
-    todos: todoState.get('todos').toJS() || dummyTodos
+    todos: todoState.get('todos').toJS() || []
   };
 };
 
