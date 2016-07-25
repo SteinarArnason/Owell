5dc2b05323ae6f0af5d32b5a984d7914d35f1060
IamStony <steinara13@ru.is>
2016-07-25 11:10:35 +0000

    added the onclick handler and changed the way props are recieved

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import Todo from './Todo';
 
-const TodoList = (list) => {
-  const { todos } = list;
-
+const TodoList = ({ todos, toggle }) => {
   return (
     <ul>
       { todos.map((t) => {
         return (
-          <li key={ t.id }>
+          <li
+            key={ t.id }
+            onClick={ () => { toggle(t.id); } } >
             <Todo todo={ t } />
           </li>
         );
       }) }
     </ul>
   );
+};
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired,
+  toggle: PropTypes.func.isRequired,
 };
 
 export default TodoList;
