c726a6d363176f72e7a9b90340e0bc1015f61018
IamStony <steinara13@ru.is>
2016-07-26 11:16:46 +0000

    removing conflicting comment

--- src/api/mockFileImporterApi.js
+++ src/api/mockFileImporterApi.js
@@ -3,7 +3,6 @@ import delay from './delay';
 
 // This file mocks a web API by working with the hard-coded data below.
 // It uses setTimeout to simulate the delay of an AJAX call.
-// All calls return promises.
 const parsedFile = {
   'head':{
     'claimantSsn':'0211614469',
